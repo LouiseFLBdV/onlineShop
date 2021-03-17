f68e0c9f9a9b10f489a1ec7d2eb3802493fbd00b
David Moraru <louiseflbdv@gmail.com>
2021-03-17 17:43:14 +0200

    temp

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    const body = req.url === '/styles.css'? fs.readFileSync('./public/styles.css'):fs.readFileSync('./public/index.html');
+    res.end(body);
+});
+
+server.listen(3000);
+
+console.log('server started');
